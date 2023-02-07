6bbc09f5b1106ceaa23f4849ccb965b1c2ecdf21
Tarun Rao <tarun_rao@hotmail.com>
2023-02-07 17:48:39 +0530

    Data Transformation completed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys,os
 from sensor.entity import config_entity
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
+from sensor.components.data_transformation import DataTransformation
 
 # def test_logger_and_exception():
 #      try:
@@ -41,5 +42,9 @@ if __name__=="__main__":
           data_validation = DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact=data_ingestion_artifact)
           data_validation_artifact = data_validation.initiate_data_validation()
           print(data_validation_artifact)
+          data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+          data_transformation = DataTransformation(data_transformation_config, data_ingestion_artifact)
+          data_transformation_artifact = data_transformation.initiate_data_transformation()
+          print(data_transformation_artifact)
      except Exception as e:
           print(e)
